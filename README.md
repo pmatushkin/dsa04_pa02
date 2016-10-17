ff87e9a45ca54e51522a86646783ee33cd88ca43
pmatushkin <pmatushkin@gmail.com>
2016-10-16 17:03:42 -0700

    feat: Pass Problem 3: Implement BetterBWMatching

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 Practice implementing Burrows–Wheeler transform and suffix arrays:
 * [`/src/bwt/BurrowsWheelerTransform.java`](src/bwt/BurrowsWheelerTransform.java): Problem 1, Construct the Burrows–Wheeler Transform of a String, construct the Burrows–Wheeler transform of a string (PASSED);
 * [`/src/bwtinverse/InverseBWT.java`](src/bwtinverse/InverseBWT.java): Problem 2, Reconstruct a String from its Burrows–Wheeler Transform, reconstructs a string from its Burrows–Wheeler transform (PASSED);
-* [`/src/bwmatching/BWMatching.java`](src/bwmatching/BWMatching.java): Problem 3, Implement BetterBWMatching, implements BetterBWMatching algorithm;
+* [`/src/bwmatching/BWMatching.java`](src/bwmatching/BWMatching.java): Problem 3, Implement BetterBWMatching, implements BetterBWMatching algorithm (PASSED);
 * [`/src/suffix_array/SuffixArray.java`](src/suffix_array/SuffixArray.java): Problem 4, Construct the Suffix Array of a String, constructs the suffix array of a string.
--- src/bwmatching/BWMatching.java
+++ src/bwmatching/BWMatching.java
@@ -33,6 +33,70 @@ public class BWMatching {
     //       from position 0 to position P inclusive.
     private void PreprocessBWT(String bwt, Map<Character, Integer> starts, Map<Character, int[]> occ_counts_before) {
         // Implement this function yourself
+        final int textLength = bwt.length();
+
+        int aCount = 0;
+        int[] aSymbols = new int[textLength + 1];
+        int cCount = 0;
+        int[] cSymbols = new int[textLength + 1];
+        int gCount = 0;
+        int[] gSymbols = new int[textLength + 1];
+        int tCount = 0;
+        int[] tSymbols = new int[textLength + 1];
+
+        for (int i = 0; i < textLength; i++) {
+            char symbolAt = bwt.charAt(i);
+
+            switch (symbolAt)
+            {
+                case 'A': {
+                    aSymbols[i] = aCount++;
+                    cSymbols[i] = cCount;
+                    gSymbols[i] = gCount;
+                    tSymbols[i] = tCount;
+                } break;
+                case 'C': {
+                    aSymbols[i] = aCount;
+                    cSymbols[i] = cCount++;
+                    gSymbols[i] = gCount;
+                    tSymbols[i] = tCount;
+                } break;
+                case 'G': {
+                    aSymbols[i] = aCount;
+                    cSymbols[i] = cCount;
+                    gSymbols[i] = gCount++;
+                    tSymbols[i] = tCount;
+                } break;
+                case 'T': {
+                    aSymbols[i] = aCount;
+                    cSymbols[i] = cCount;
+                    gSymbols[i] = gCount;
+                    tSymbols[i] = tCount++;
+                } break;
+                default: {
+                    aSymbols[i] = aCount;
+                    cSymbols[i] = cCount;
+                    gSymbols[i] = gCount;
+                    tSymbols[i] = tCount;
+                } break;
+            }
+        }
+
+        aSymbols[textLength] = aCount;
+        cSymbols[textLength] = cCount;
+        gSymbols[textLength] = gCount;
+        tSymbols[textLength] = tCount;
+
+        starts.put('$', 0);
+        starts.put('A', aCount == 0 ? -1 : 1);
+        starts.put('C', cCount == 0 ? -1 : 1 + aCount);
+        starts.put('G', gCount == 0 ? -1 : 1 + aCount + cCount);
+        starts.put('T', tCount == 0 ? -1 : 1 + aCount + cCount + gCount);
+
+        occ_counts_before.put('A', aSymbols);
+        occ_counts_before.put('C', cSymbols);
+        occ_counts_before.put('G', gSymbols);
+        occ_counts_before.put('T', tSymbols);
     }
 
     // Compute the number of occurrences of string pattern in the text
@@ -40,6 +104,34 @@ public class BWMatching {
     // information we get from the preprocessing stage - starts and occ_counts_before.
     int CountOccurrences(String pattern, String bwt, Map<Character, Integer> starts, Map<Character, int[]> occ_counts_before) {
         // Implement this function yourself
+        int top = 0;
+        int bottom = bwt.length() - 1;
+
+        int index = pattern.length() - 1;
+        while (top <= bottom) {
+            if (index >= 0) {
+                char symbolAt = pattern.charAt(index--);
+
+                if (symbolAt == '$') {
+                    continue;
+                }
+
+                int[] occ_count = occ_counts_before.get(symbolAt);
+
+                int countAtTop = occ_count[top];
+                int countAtBottom = occ_count[bottom + 1];
+
+                if (countAtBottom > countAtTop) {
+                    top = starts.get(symbolAt) + countAtTop;
+                    bottom = starts.get(symbolAt) + occ_count[bottom + 1] - 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return bottom - top + 1;
+            }
+        }
+
         return 0;
     }
 
